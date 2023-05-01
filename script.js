84d0e28ad1a1b16069b5d95508a3bc586c5387ea
SaadHussein <saadwithyara@gmail.com>
2023-05-01 10:07:18 +0300

    Commited

---
+++ script.js
@@ -0,0 +1,95 @@
+const word = document.getElementById("word");
+const submitButton = document.getElementById("submit");
+const meaningList = document.getElementById("meaning-list");
+const verbMeaningList = document.getElementById("verb-meaning-list");
+const about = document.querySelector(".about");
+const myWord = document.querySelector(".wooord");
+const spelling = document.querySelector(".spelling");
+const audioo = document.querySelector(".audioo");
+const synonymsWord = document.querySelector(".synonyms-word");
+const sourceLink = document.querySelector(".source-link");
+
+function getData(word) {
+  fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data[0]);
+      if (data === undefined || data.title === "No Definitions Found") {
+        alert("This Word is not Correct");
+        throw new Error("This Word is not Correct");
+      }
+      makeInfoVisible();
+      setAboutInformation(data[0]);
+      audioo.addEventListener("click", () => {
+        setAudio(data[0]);
+      });
+      setMeaningSection(data[0]);
+      setVerbList(data[0]);
+      setLink(data[0]);
+    });
+}
+
+submitButton.addEventListener("click", () => {
+  if (word.value !== "") {
+    getData(word.value);
+  } else {
+    alert("Please Enter a Correct Word.");
+  }
+});
+
+function makeInfoVisible() {
+  about.style.display = "block";
+}
+
+function setAboutInformation(data) {
+  myWord.innerHTML = data.word;
+  spelling.innerHTML = data.phonetics[1].text
+    ? data.phonetics[1].text
+    : data.phonetic;
+}
+
+function setAudio(data) {
+  const audio = new Audio(data.phonetics[0].audio);
+  audio.play();
+}
+
+function setMeaningSection(data) {
+  let listItems = "";
+  meaningList.innerHTML = "";
+  synonymsWord.innerHTML = "";
+  data.meanings[0].definitions.forEach((item) => {
+    listItems += `
+      <li class="meaning-list-item">${item.definition}</li>
+    `;
+  });
+
+  meaningList.innerHTML = listItems;
+
+  if (data.meanings[0].synonyms.length !== 0) {
+    synonymsWord.innerHTML = data.meanings[0].synonyms[0];
+  } else {
+    synonymsWord.innerHTML = "There is no Synonyms";
+  }
+}
+
+function setVerbList(data) {
+  let listItems = "";
+  verbMeaningList.innerHTML = "";
+
+  data.meanings[1].definitions.forEach((item) => {
+    listItems += `
+    <li class="verb-meaning-list-item">
+      <p class="verb-meaning-list-item-header">${item.definition}</p>
+      <p class="verb-meaning-list-item-item">${
+        item.example ? item.example : ""
+      }</p>
+    </li>
+    `;
+  });
+
+  verbMeaningList.innerHTML = listItems;
+}
+
+function setLink(data) {
+  sourceLink.innerHTML = `${data.sourceUrls[0]}`;
+}
